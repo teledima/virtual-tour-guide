8c266f812be4e99106d0be328703704ee6ea23f3
teledima <teledima00@gmail.com>
2022-05-14 19:51:56 +0500

    remove test repository

--- frontend_flutter_router/android/app/src/main/kotlin/com/example/frontend_flutter_router/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.frontend_flutter_router
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend_flutter_router/lib/models.dart
+++
@@ -1,7 +0,0 @@
-class Item {
-  String id;
-  String title;
-  String? detail;
-
-  Item({required this.id, required this.title, this.detail});
-}
