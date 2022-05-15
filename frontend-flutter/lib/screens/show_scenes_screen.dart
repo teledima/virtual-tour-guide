7c941273edf1b712edaf7b53620a338f9cee726f
teledima <teledima00@gmail.com>
2022-05-15 21:01:41 +0500

    Add scenes to page

--- frontend-flutter/lib/screens/show_scenes_screen.dart
+++ frontend-flutter/lib/screens/show_scenes_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 // Application
 import 'package:frontend_flutter/models.dart';
+import 'package:frontend_flutter/widgets/scene_card.dart';
 
 class ShowScenesScreen extends StatefulWidget {
   final TourDetail tour;
@@ -15,10 +16,15 @@ class ShowScenesScreen extends StatefulWidget {
 }
 
 class ShowScenesScreenState extends State<ShowScenesScreen> {
+  @override
   Widget build(BuildContext context) {
+    final List<Widget> children = [
+      for (var scene in widget.tour.scenes!)
+        SceneCard(scene: scene)
+    ];
     return Scaffold(
       appBar: AppBar(title: const Text('Scenes')),
-      body: const Center(child: Text('Show scenes')),
+      body: GridView.extent(maxCrossAxisExtent: 360, children: children,),
     );
   }
 }
