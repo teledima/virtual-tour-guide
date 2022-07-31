3352552e77cf40693f0ae2c5ad1d7ebe079eaf08
teledima <teledima00@gmail.com>
2022-07-31 18:00:25 +0500

    feat[backend]: refactor tour controller

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,76 +1,55 @@
 const passport = require('passport')
 const express = require('express')
-const { ObjectId } = require('mongodb')
+const { CastError } = require('mongoose')
 
 
-const { mongo } = require('../utils')
-let { mongoClient, tourCollection } = mongo
+const { logger } = require('../utils')
+const { Tour } = require('../models')
 
 const router = express.Router()
 router.use(passport.authenticate('jwt', {session: false}))
 
 router.post('/', async(req, res) => {
-    mongoClient = await mongoClient.connect();
-
-    const result = await tourCollection.insertOne({...req.body, "_id": new ObjectId()});
-    res.status(200).send(result);
+    const tour = await new Tour({...req.body, owner: req.user._id}).save()
+    res.status(200).send(tour.toJSON({ versionKey: false }));
 });
 
 router.get('/all', async(req, res) => {
-    mongoClient = await mongoClient.connect();
-    
-    const cursor = tourCollection.find({});
-    res.status(200).send(await cursor.toArray());
+    res.status(200).send(await Tour.find({
+        owner: req.user._id
+    }));
 });
 
 router.get('/:tour', async(req, res) => {
-    const tour = req.params.tour;
-    mongoClient = await mongoClient.connect()
+    const tourId = req.params.tour;
 
     try {
-        if (ObjectId.isValid(tour)) {
-            const cursor = tourCollection.aggregate([
-                { $match: { _id: new ObjectId(tour) } },
-            ])
-
-            let result = {}
-            res = res.status(200)
-            if (await cursor.hasNext())
-                result = await cursor.next()
-            res.send(result)
+        const tour = await Tour.findById(tourId)
+        res.status(200).send(tour.toJSON({ versionKey: false }))
+    } catch (e) {
+        logger.error(JSON.stringify(e))
+        if (e instanceof CastError) {
+            res.sendStatus(404)
+        } else {
+            res.sendStatus(500)
         }
-        else {
-            res.status(400).send('incorrect id')
-        }
-    }
-    catch(error) {
-        console.log(error)
-    }
-    finally {
-        await mongoClient.close()
     }
 })
 
 router.patch('/', async(req, res) => {
-    mongoClient = await mongoClient.connect();
-
-    const body = req.body['updateBody']
-    let updateBody = {}
-    if ('title' in body) {
-        updateBody['title'] = body['title'];
-    } 
-    if ('default' in body) {
-        updateBody['default'] = {};
-        if ('firstScene' in body['default']) {
-            updateBody['default']['firstScene'] = new ObjectId(body['default']['firstScene'])
+    try {
+        await Tour.findByIdAndUpdate(req.body.tourId, req.body.updateBody)
+        const tour = await Tour.findById(req.body.tourId)
+        res.status(200).send(tour.toJSON({ versionKey: false }));
+    } catch(e) {
+        console.log(e)
+        logger.error(JSON.stringify(e))
+        if (e instanceof CastError) {
+            res.sendStatus(404)
+        } else {
+            res.sendStatus(500)
         }
     }
-
-    const updateRes = await tourCollection.updateOne(
-        { _id: new ObjectId(req.body['tourId']) },
-        { $set: updateBody }
-    );
-    res.status(200).send(updateRes);
 })
 
 module.exports = router
